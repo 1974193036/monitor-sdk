623e3cc41c21382b8adc62dab50b3d4e9c8dec1c
ssy <1974193036@qq.com>
2025-01-06 23:58:24 +0800

    feat: 页面性能监控

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { setConfig } from './config'
 import error from './error'
+import performance from './performance'
 
 const monitor = {
   init(options = {}) {
     // console.log('init', options)
     setConfig(options)
     error()
+    performance()
   },
 }
 
