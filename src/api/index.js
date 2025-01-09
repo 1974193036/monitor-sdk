e6fb242ac47420b235e43d4165ca7fc894bcf394
ssy <1974193036@qq.com>
2025-01-10 00:23:49 +0800

    feat: api远程请求数据采集

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+import fetch from './fetch'
+import xhr from './xhr'
+
+export default function api() {
+  xhr()
+  fetch()
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { autoTracker, tracker } from './action'
+import api from './api'
 import { onVueRouter, pageChange, pageStayTime, pv } from './behavior'
 import { setConfig } from './config'
 import error from './error'
@@ -7,11 +8,12 @@ import performance from './performance'
 const monitor = {
   init(options = {}) {
     // console.log('init', options)
-    setConfig(options)
-    error()
-    performance()
-    autoTracker()
-    pv()
+    setConfig(options) // 配置全局参数
+    error()// 错误监听处理
+    performance() // 性能监听处理
+    autoTracker()// 自动埋点
+    pv() // page view
+    api() // api远程请求数据采集
   },
   tracker,
   pageStayTime,
